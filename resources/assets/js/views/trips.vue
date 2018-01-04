670a7e4bf043a4f9df7edd2ed07df32e2f9a5c7a
Илья <>
2018-01-04 21:17:07 +0600

    ok

--- resources/assets/js/views/trips.vue
+++ resources/assets/js/views/trips.vue
@@ -1,11 +1,15 @@
 ﻿<template>
 <div>
-	<center>
+    <b-container>
+    <b-row class="text-center">
+	<b-col>
 	<h3 style="margin-top:20px;text-decoration:underline">Поездки на август</h3>
 	<h5>Сергей едет в тайланд</h5>
 	<h5>Наталья летит в турцию</h5>
 	<h5>Михаил летит в турцию</h5>
-	<b-btn class="mt-1" variant="outline-success" block @click="hideModalReg">Добавить себя</b-btn>
-	</center>
+	<b-button variant="primary">Добавить себя</b-button>
+	</b-col>
+    </b-row>
+    </b-container>
 </div>
 </template>
