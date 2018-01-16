213cbe936e5b0ab2b8fe5b2809a2e28141ef09ba
heksen84 <heksen84@yandex.ru>
2018-01-16 22:20:41 +0600

    ok

---
+++ resources/assets/js/views/error.vue
@@ -0,0 +1,18 @@
+﻿<template>
+<div>
+<NavBar></NavBar>
+</div>
+</template>
+<script type="text/javascript">
+
+import Vue from 'vue'
+import NavBar from './navbar.vue'
+
+export default {
+	data () {
+    return 	{
+			}
+	},
+	components: { NavBar }
+}
+</script>
