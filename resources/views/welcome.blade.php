7a07f63e897cdf0e6aabbb79ad1d7271fb000018
heksen84 <heksen84@yandex.ru>
2018-01-24 22:08:57 +0600

    ok

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html lang="{{ config('app.locale') }}">
     <head>
+        <title>Сайт знакомств Love In Travels</title>
+        <meta name="keywords" content="сайт знакомств, знакомства, путешествия, путешествие, роман, любовный роман, флирт, любовь, поиск, вторая половина, отношения" />
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="love in travels" content="Awesome Description Here">
-        <title>Love In Travels - люби и путешествуй!</title>
+	      <meta name="love in travels" content="Сайт знакомств Love In Travels">
         <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}">
     </head>
     <body>
