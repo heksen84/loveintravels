09a509b419e9b5be229bf6cd9fc5032196248a1d
heksen84 <heksen84@yandex.ru>
2018-02-14 21:25:38 +0600

    ok

--- routes/api.php
+++ routes/api.php
@@ -6,9 +6,11 @@ Route::post('signup', 'AuthController@signup');
 Route::post('test',   'AuthController@test');
 Route::resource('recipes', 'RecipeController');
 
-Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
-Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
-Route::get('password/reset/{token?}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
-Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+/*
+        $this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+        $this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+        $this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+        $this->post('password/reset', 'Auth\ResetPasswordController@reset');
+*/
 
 ?>
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
+//Auth::routes();
+
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token?}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+
 Route::get('/{any}', function () {
     return view('welcome');
 })->where(['any' => '.*']);
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
