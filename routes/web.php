e8a7663098ed6a9cfa99c9654ec074aa1ff6c4fc
heksen84 <heksen84@yandex.ru>
2018-02-21 20:19:36 +0600

    ok

--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,14 @@
 Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
 Route::get('password/reset/{token?}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset', 'Auth\ResetPasswordController@reset');*/
-
-
 //Route::post('password/email', 'Auth\ResetPasswordController@postEmail');
 //Route::post('password/reset', 'Auth\ResetPassController@postReset');
 
 Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
 Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
 
-// редиректю на vue вьюху
+// редиректю в начале на родной контроллер, а потом на vue roter на vue вьюху
+// (это hack, как сделать редирект сходу?)
 Route::get('password/reset/{token?}', function ($token) {
     return redirect('password/resetpass/'.$token);
 })->name('password.reset');
